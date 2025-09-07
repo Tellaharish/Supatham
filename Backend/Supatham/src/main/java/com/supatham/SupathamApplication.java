5df82d36b6725fa37a1c49cff9751175450d22a0
unknown <hariahkumarrdy@gmail.com>
2025-09-07 20:43:26 +0530

    first commit folders created for backend and front end

---
+++ Backend/Supatham/src/main/java/com/supatham/SupathamApplication.java
@@ -0,0 +1,13 @@
+package com.supatham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupathamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupathamApplication.class, args);
+	}
+
+}
---
+++ Backend/Supatham/src/main/java/com/supatham/entity/Users.java
@@ -0,0 +1,8 @@
+package com.supatham.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Users {
+
+}
